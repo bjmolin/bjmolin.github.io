830f11e117309d55697ed4b9bdf7cce0e2bac68f
bjmolin <bjmolin@g.clemson.edu>
2026-07-07 11:23:20 -0400

    navbar update

--- projects/unused/group/7/car-display.php
+++ projects/unused/group/7/car-display.php
@@ -15,7 +15,7 @@ $selected_cars = isset($_SESSION['selected_cars']) ? $_SESSION['selected_cars'] 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Car Display Page</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="../css/base.css">
     <link href="../css/navbar.css" rel="stylesheet" type="text/css">
     <script src="../navbarLoader.js"></script>
--- projects/unused/group/7/car-selection.php
+++ projects/unused/group/7/car-selection.php
@@ -7,7 +7,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Car Selection Page</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="../css/base.css">
     <link href="../css/navbar.css" rel="stylesheet" type="text/css">
     <script src="../navbarLoader.js"></script>
